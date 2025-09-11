48c194fabd96071bf3e854e704b1fbe541f295ce
joxquinnn <joxquinnn1137@gmail.com>
2025-09-11 03:41:42 -0300

    Implementaciónes columna chats
    se implementaron diversas secciones de la columna de chats partiendo por la division de grupos y chats, a su vez se añadió la barra de busqueda con dos botones para la busqueda misma y chats favoritos (falta ajustar tamaño) y por ultimo el div y estilo de los chat que irán en un futuro.

--- Frontend/waza/src/components/layout/NavBar.tsx
+++ Frontend/waza/src/components/layout/NavBar.tsx
@@ -1,8 +1,32 @@
-import styles from '@/styles/NavBar.module.css'
+import styles from "@/styles/NavBar.module.css"
+import { FcSearch } from "react-icons/fc";
+import { MdFavoriteBorder } from "react-icons/md";
 
 export default function NavBar () {
     return (
-        <nav className={styles.navContainer}>
-        </nav>
-    )
+        <div className={styles.navContainer}>
+            <div className={styles.topSection}> 
+            <button className={styles.groupChatsButton}>Grupos</button>
+            <button className={styles.groupChatsButton}>Chats</button>
+            </div>   
+
+            <div className={styles.searchSection}>
+                <input
+                    type="text"
+                    placeholder="Buscar Contactos"
+                    className={styles.searchInput}
+                />
+                <button className={styles.searchButton}>
+                    <FcSearch />   
+                </button>
+                <button className={styles.favoriteButton}>
+                    <MdFavoriteBorder />
+                </button>
+            </div>
+
+            <div className={styles.chatListSection}>
+                <p>chat chat chat chat chat chat y mas chats...</p>
+            </div>
+        </div>
+    );
 }
