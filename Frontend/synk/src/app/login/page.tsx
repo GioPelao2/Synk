6b48f01051c67a52b7fa4f33023c8b46fb305975
joxquinnn <joxquinnn1137@gmail.com>
2025-10-01 22:27:42 -0300

    estructuración login-page

--- Frontend/synk/src/app/login/page.tsx
+++ Frontend/synk/src/app/login/page.tsx
@@ -1,1 +1,13 @@
-import styles from '@/app/login/page.module.css'
+'use client';
+import React from 'react';
+import styles from '@/app/login/page.module.css';
+import LoginView from '@/components/auth/LoginView';
+import RegisterView from '@/components/auth/RegisterView';
+import SuccessView from '@/components/auth/SuccessView';
+
+export default function LoginPage(){
+    return (
+        <div className={styles.maincontainer}>
+        </div>
+    )
+}
---
+++ Frontend/synk/src/components/auth/LoginView.tsx
@@ -0,0 +1,3 @@
+import React, { useState} from 'react';
+import styles from '@/styles/auth.module.css'
+import Link from 'next/link';
